280df74841b2f9f37040d87a0c0605e1b32aca3c
Vivek <159415297+Vivekbhatt02@users.noreply.github.com>
2026-05-05 20:05:07 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            if(ch=='{' || ch=='(' || ch=='['){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+            Character top = stack.pop();
+
+            if(top=='{' && ch!='}' || top=='(' && ch!=')' || top=='[' && ch!=']'){
+                return false;
+            }
+            
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
