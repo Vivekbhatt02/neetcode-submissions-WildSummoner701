a84f474166df9f9c305c4c88a3fcfbab19650e55
Vivek <159415297+Vivekbhatt02@users.noreply.github.com>
2026-06-18 07:32:56 +0530

    Add: duplicate-integer - submission-35

---
+++ Data Structures & Algorithms/duplicate-integer/submission-35.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const set =  new Set();
+
+        for(let item of nums){
+            if(set.has(item)){
+                return true;
+            }
+            set.add(item);
+        }
+
+        return false;
+    }
+}
